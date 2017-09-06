246659e801f294d9dabc8f563c658bfe0f947e39
dweb1 <david.p.weber@gmail.com>
2017-09-06 12:11:32 -0400

    Added edit and update routes and views - can now edit creature

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -19,6 +19,16 @@ class CreaturesController < ApplicationController
         end
     end
 
+    def edit
+        @creature = Creature.find(params[:id])
+    end
+
+    def update
+        @creature = Creature.find(params[:id])
+        @creature.update(creature_params)
+        redirect_to creature_path(@creature)
+    end
+
     private
     def creature_params
         params.require(:creature).permit(:name, :description)
